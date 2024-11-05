8e55b613c50882204283a5e496dca3153abed895
muhammed-kutty <mkkunnathodi@gmail.com>
2024-11-05 21:52:27 +0530

    console cahge in middleware

--- middleware/Jwt.js
+++ middleware/Jwt.js
@@ -8,7 +8,7 @@ const JWT_SECRET =process.env.JWT_SECRET;
 
 export const verifyToken = (req, res, next) => {
   const token = req.headers['authorization']?.split(' ')[1];
-  console.log(req.headers) 
+  console.log("in jwt tttttttttttttttttttttttttttt",req.headers['authorization']) 
   if (!token) {
     return res.status(403).json({isValied:false, message: 'Token is required' });
   }
